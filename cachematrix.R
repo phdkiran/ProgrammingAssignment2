1d9ef1fcb6cb7d5629f8fb9fff8610e99cca0718
phdkiran <phdkiran@users.noreply.github.com>
2014-11-22 00:49:20 -0700

    Update cachematrix.R
    CacheMatrix updated and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Find if the input matrix is cached. Return cached matrix if found in the getCache function. Otherwise 
+## find inverse and store it using setCache and return matrix inverse
 
-## Write a short comment describing this function
+## 
+## Find if the input matrix is cached. Return cached matrix if found in the getCache function. Otherwise 
+## find inverse and store it using setCache and return matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+mat <- NULL
+get <- function () x;
+setCache <- function (cache) mat <<- cache;
+getCache <- function () mat;
+list(get=get, setCache=setCache, getCache=getCache)
 }
 
 
-## Write a short comment describing this function
+## Find if the input matrix is cached. Return cached matrix if found in the getCache function. Otherwise 
+## find inverse and store it using setCache and return matrix inverse
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+  val <- x$getCache()
+  if(!is.null(val)){
+    print("Getting Cached matrix")
+    val
+  }
+  val <- solve (x$get(), ...);
+  x$setCache(val);
+  val
 }
